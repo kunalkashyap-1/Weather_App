d4cb8c1c696f90bda46dc3eaffd076935732b115
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-09-11 16:49:18 +0530

    initial commit not so initial tho

---
+++ index.js
@@ -0,0 +1,34 @@
+const time_data=document.getElementById("time");
+const date_data=document.getElementById("date");
+const currentWeatherItems=document.getElementById("current_weather_data");
+const timeZone=document.getElementById("time_zone");
+const country=document.getElementById("country");
+const forecastItem=document.getElementById("forecast_item");
+const currentTemp=document.getElementById("today");
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY="f68ab141beada1ae2477e32a1215699e";
+
+setInterval(function(){
+    const time=new Date();
+    const month=time.getMonth();
+    const date =time.getDate();
+    const day=time.getDay();
+    const hour=time.getHours();
+    const minutes=time.getMinutes();
+    const ampm=hour>=12?"PM":"AM";
+    time_data.innerHTML=(hour<10?"0"+hour:hour) + " : " + (minutes<10?"0"+minutes:minutes) + `<span id="AM-PM"> ${ampm}</span>`;
+    date_data.innerHTML=days[day] +", "+ date + " " + months[month];
+},1000)
+
+
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success)=>{
+        let {latitude,longitude}=success.coords;//this is object destructuring 
+        
+    })
+}
+
+getWeatherData();
